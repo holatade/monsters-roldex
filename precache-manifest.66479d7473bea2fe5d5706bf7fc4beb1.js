d7f4cad7df5123f74da0be0510c62686eefb8877
Hassan Hassan <hassan.olatade.hh@gmail.com>
2020-05-30 10:52:21 +0100

    Updates

---
+++ precache-manifest.66479d7473bea2fe5d5706bf7fc4beb1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90407f6db20e118cd353e442fe75e07d",
+    "url": "/monsters-roldex/index.html"
+  },
+  {
+    "revision": "f00b975d521282b74797",
+    "url": "/monsters-roldex/static/css/main.f36176a4.chunk.css"
+  },
+  {
+    "revision": "ded29ce0b905d53b3146",
+    "url": "/monsters-roldex/static/js/2.d2b43323.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-roldex/static/js/2.d2b43323.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f00b975d521282b74797",
+    "url": "/monsters-roldex/static/js/main.661fd996.chunk.js"
+  },
+  {
+    "revision": "5b353f157a76b08d41e1",
+    "url": "/monsters-roldex/static/js/runtime-main.8f12e8d5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-roldex/static/media/logo.5d5d9eef.svg"
+  }
+]);
